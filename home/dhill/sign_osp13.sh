f7c262a8e6ca68d263964761a924df46820e92ab
David Hill <dhill@redhat.com>
2026-02-18 08:48:35 -0500

    Some new signed packages in sign_osp13.sh

--- home/dhill/sign_osp13.sh
+++ home/dhill/sign_osp13.sh
@@ -1,8 +1,11 @@
 #brew tag-pkg rhos-13.0-rhel-7-hotfix python-os-brick-2.3.9-8.el7ost
 #brew call signBuild python-os-brick-2.3.9-8.el7ost beta2
 
-brew tag-pkg rhos-13.0-rhel-7-hotfix openstack-nova-17.0.13-38.el7ost
-brew call signBuild openstack-nova-17.0.13-38.el7ost beta2
+#brew tag-pkg rhos-13.0-rhel-7-hotfix openstack-nova-17.0.13-38.el7ost
+#brew call signBuild openstack-nova-17.0.13-38.el7ost beta2
+
+brew tag-pkg rhos-13.0-rhel-7-hotfix python-glance-store-0.23.1-0.20190916165254.cc7ecc1.el7ost
+brew call signBuild python-glance-store-0.23.1-0.20190916165254.cc7ecc1.el7ost beta2
 
 echo openstack-nova-17.0.13-38.el7ost
 echo path = http://download.eng.bos.redhat.com/brewroot/packages/openstack-nova/17.0.12/12.el7ost/data/signed/f21541eb/noarch/
