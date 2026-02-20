b86483eeda3270534be2ef388dc865136eac3495
David Hill <dhill@redhat.com>
2026-02-20 10:38:16 -0500

    Preserve sparse in libvirt

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 sudo tar uvf /home/dhill/jenkins.tar /var/lib/jenkins
 sudo tar uvf /home/dhill/cases.tar /cases
-sudo tar uvf /home/dhill/libvirt.tar /var/lib/libvirt
+sudo tar -Suvf /home/dhill/libvirt.tar /var/lib/libvirt
