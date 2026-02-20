88a24ca1b8c3eb62acc589e373ccf17d0dfbc259
David Hill <dhill@redhat.com>
2026-02-20 10:39:13 -0500

    Change owner on backup files

--- backup.sh
+++ backup.sh
@@ -1,3 +1,4 @@
 sudo tar uvf /home/dhill/jenkins.tar /var/lib/jenkins
 sudo tar uvf /home/dhill/cases.tar /cases
 sudo tar -Suvf /home/dhill/libvirt.tar /var/lib/libvirt
+sudo chown dhill /home/dhill/libvirt.tar /home/dhill/cases.tar /home/dhill/jenkins.tar
