f17df041272a7e525cbc04f4c3c2ded3b1720591
David Hill <dhill@redhat.com>
2026-08-06 12:28:06 -0400

    Sync files only if they are present after deja-dup execution

--- install_on_csb.sh
+++ install_on_csb.sh
@@ -15,10 +15,6 @@ dconf write /org/gnome/Weather/locations "[<(uint32 2, <('Montreal', 'CYUL', tru
 if ! rpm -qi virt-manager > /dev/null; then
   sudo yum install -y --skip-unavailable terminator vim snap shairport-sync vlc thunderbird net-snmp google-chrome-unstable gnome-classic-session git-lfs hexchat psutils glibc.i686 virt-manager gnome-session-xsession openshift-clients chromium
 fi
-if [ -d /home/dhill_restore/Documents ]; then
-  rsync -avgo --remove-source-files /home/dhill_restore/ /home/dhill/
-  sudo find /home/dhill_restore/ -depth -type d -empty -delete
-fi
 
 flatpak install anydesk
 
@@ -132,4 +128,8 @@ fi
 if [[ $(hostname) == moriarty.orion ]] && [ ! -e .restored ]; then
   deja-dup
   touch .restored
+  if [ -d /home/dhill_restore/Documents ]; then
+    rsync -avgo --remove-source-files /home/dhill_restore/ /home/dhill/
+    sudo find /home/dhill_restore/ -depth -type d -empty -delete
+  fi
 fi
